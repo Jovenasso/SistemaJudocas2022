b58cd75c87c52ab51996029077040aca73aacdba
Bruno Jovenasso <80861112+Jovenasso@users.noreply.github.com>
2022-11-25 15:03:46 -0300

    Adição do validator de professor

---
+++ src/org/fpij/jitakyoei/model/validator/ProfessorValidator.java
@@ -0,0 +1,9 @@
+package org.fpij.jitakyoei.model.validator;
+
+import org.fpij.jitakyoei.model.beans.Professor;
+
+public class ProfessorValidator implements Validator<Professor>{
+	public boolean validate(Professor obj) {
+		return true;
+	}
+}
